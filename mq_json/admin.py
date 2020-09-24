2c6cf9f78e4606b3906440399aada8e76c59e8de
Григорьев Ростислав <43699715+Irlirion@users.noreply.github.com>
2020-09-24 20:23:58 +0300

    Add admin interface

--- mq_json/admin.py
+++ mq_json/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from mq_json.models import Client, Message
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['uuid']
+    ordering = ['uuid']
+    search_fields = ['uuid']
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender_uuid', 'recipient_uuid', 'message')
+    ordering = ['sender_uuid']
+    search_fields = ('sender_uuid', 'recipient_uuid')
